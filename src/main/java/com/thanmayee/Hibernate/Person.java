12ce0ce96dfcb7b353a875256fef2afb290dcf72
Thanmayee167 <thanmayee167@gmail.com>
2025-01-10 22:38:54 +0530

    Added code to demonstrate concept of OneToMany and ManyToOne BiDirectional Hibernate Relation between entities using MappedBy to tell which Entity will own the FORIEGN KEY

--- src/main/java/com/thanmayee/Hibernate/Person.java
+++ src/main/java/com/thanmayee/Hibernate/Person.java
@@ -14,7 +14,7 @@ public class Person {
 	private String name;
 	private int age;
 
-	@OneToMany
+	@OneToMany(mappedBy = "person")
 	private List<Laptop> laptops = new ArrayList<Laptop>();
 
 	public int getSno() {
