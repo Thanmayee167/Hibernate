c557d71b453620ed81f0b990164a1ee6218e90ed
Thanmayee167 <thanmayee167@gmail.com>
2025-02-17 16:33:09 +0530

    Demonstrated how to use HQL(Hibernate Query Language) for select queries

--- src/main/java/com/thanmayee/Hibernate/App.java
+++ src/main/java/com/thanmayee/Hibernate/App.java
@@ -1,5 +1,7 @@
 package com.thanmayee.Hibernate;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -7,8 +9,6 @@ import org.hibernate.query.Query;
 
 public class App {
 	public static void main(String[] args) {
-
-		Person person;
 
 		Configuration con = new Configuration().configure().addAnnotatedClass(Person.class)
 				.addAnnotatedClass(Laptop.class);
@@ -19,27 +19,56 @@ public class App {
 
 		session1.beginTransaction();
 
-		Query q1 = session1.createQuery("from Person where sno=1");
-		q1.setCacheable(true);
-		person = (Person) q1.uniqueResult();
-		System.out.println(person);
-		// System.out.println(person.getLaptops());
+//		insert values
+//		Random random = new Random();
+//		for (int i = 1; i <= 10; i++) {
+//			Person person = new Person();
+//			person.setSno(i + 1);
+//			person.setName("Name" + i);
+//			person.setAge(random.nextInt(100));
+//			session1.persist(person);
+//		}
+
+//		fetches list of persons
+//		Query query = session1.createQuery("from Person");
+//		List<Person> persons = query.list();
+//		for (Person person : persons) {
+//			System.out.println(person);
+//		}
+
+// 		fetches specific record
+//		Query query = session1.createQuery("from Person where sno=10");
+//		Person person = (Person) query.uniqueResult();
+//		System.out.println(person);
+
+// 		fetches specific record using Select query
+//		Query query = session1.createQuery("Select sno,name,age from Person where sno=10");
+//		Object[] person = (Object[]) query.uniqueResult();
+//		System.out.println("Person [sno=" + person[0] + ", name=" + person[1] + ", age=" + person[2] + "]");
+
+//		fetches records that contains age>10
+//		Query query = session1.createQuery("Select sno,name,age from Person where age>10");
+//		List<Object[]> persons = (List<Object[]>) query.list();
+//		for (Object[] person : persons) {
+//			System.out.println("Person [sno=" + person[0] + ", name=" + person[1] + ", age=" + person[2] + "]");
+//		}
+
+// 		fetches average age of all the records
+//		Query query = session1.createQuery("Select AVG(age) from Person");
+//		Double averageMarks = (Double) query.uniqueResult();
+//		System.out.println("Average marks=" + averageMarks);
+
+//		fetches records that contains age>10
+		int age = 10;
+		Query query = session1.createQuery("Select sno,name,age from Person where age> :age");
+		query.setParameter("age", age);
+		List<Object[]> persons = (List<Object[]>) query.list();
+		for (Object[] person : persons) {
+			System.out.println("Person [sno=" + person[0] + ", name=" + person[1] + ", age=" + person[2] + "]");
+		}
 
 		session1.getTransaction().commit();
 		session1.close();
-
-		Session session2 = sf.openSession();
-
-		session2.beginTransaction();
-
-		Query q2 = session2.createQuery("from Person where sno=1");
-		q2.setCacheable(true);
-		person = (Person) q2.uniqueResult();
-		System.out.println(person);
-		// System.out.println(person.getLaptops());
-
-		session2.getTransaction().commit();
-		session2.close();
 
 	}
 }
